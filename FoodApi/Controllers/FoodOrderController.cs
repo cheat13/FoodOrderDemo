e43516af34f78d879de1a1c5558ab81ab1f4f1ce
Kritsana <tragoolphrom@gmail.com>
2019-03-20 22:58:50 +0700

    implement Models

---
+++ FoodApi/Controllers/FoodOrderController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FoodOrderController : ControllerBase
+    {
+        public static List<Food> Menu = new List<Food>
+        {
+            new Food{
+                Id = Guid.NewGuid().ToString(),
+                Name = "สเต๊กเนื้อ",
+                Price = 129,
+                Description = "เนื้อชั้นดี เบอร์ตอง",
+                CookingTime = 10
+            },
+            new Food{
+                Id = Guid.NewGuid().ToString(),
+                Name = "สเต๊กหมู",
+                Price = 99,
+                Description = "หมูชั้นดี เบอร์ตอง",
+                CookingTime = 10
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Food>> GetMenu()
+        {
+            return Menu;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Food> GetFood(string id)
+        {
+            return Menu.FirstOrDefault(it => it.Id == id);
+        }
+
+    }
+}
---
+++ FoodApi/Models/Food.cs
@@ -0,0 +1,14 @@
+namespace FoodApi.Models
+{
+    public class Food
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public double CookingTime { get; set; }
+        public string Image { get; set; }
+        public int Amount { get; set; }
+        public string Comment { get; set; }
+    }
+}
